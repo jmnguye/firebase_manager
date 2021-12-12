5364033b323015f6f40c39a3c861f7f70c5c7835
mxp <maxplayer@free.fr>
2021-12-12 14:02:08 +0100

    add node count limitation

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import firebase_admin
 import os
+import json
 from firebase_admin import credentials
 from firebase_admin import db
 
@@ -41,11 +42,11 @@ my_value3 = {
     'timestamp': {'.sv': 'timestamp'}
 }
 
-ref.set(my_value)
-ref.push(my_value2)
-ref.push(my_value3)
+# ref.push(my_value)
+# ref.push(my_value2)
+#ref.push(my_value3)
 
-print(ref.get())
+#print(json.dumps(ref.get(), indent=4, sort_keys=True))
 
 
 def update():
@@ -59,4 +60,29 @@ def add():
 def delete():
     pass
 
-# print(ref.)
+
+'''
+i want to limit entries up to let's say 2 for the moment
+in case there are more, remove the oldest one
+data are filled like a queue
+get the size of ref
+'''
+shallowed = ref.get(shallow=True) # get only keys
+json_sorted = json.dumps(shallowed, sort_keys=True) # this trick help me sort key
+nodes_sorted = json.loads(json_sorted) # build python objects from json output sorted
+#print(type(nodes_sorted))
+#print(db.reference(f'/commits/{list(nodes_sorted.keys())[0]}').get())
+while len(shallowed) > 10:
+    top_node = list(nodes_sorted.keys())[0]
+    node = db.reference(f'/commits/{top_node}')
+    print(node.get())
+    node.delete()
+    shallowed = ref.get(shallow=True)
+    
+
+#for node in nodes_sorted:
+#        node_ref = db.reference(f'/commits/{node}')
+#        print(node_ref.get())
+
+
+print(len(shallowed))
